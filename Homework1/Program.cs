4ab0d70dad6e33d447b435e1a105643c5fb6edf7
tricksteropc <mi070607r@gmail.com>
2026-03-15 22:07:57 +0300

    Homework 1 - Damerau Levenstein

---
+++ Homework1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Введите первую строку (или 'exit' для выхода):");
+            string str1 = Console.ReadLine();
+
+            if (str1.ToLower() == "exit")
+                break;
+
+            Console.WriteLine("Введите вторую строку:");
+            string str2 = Console.ReadLine();
+
+            int distance = Distance(str1, str2);
+
+            Console.WriteLine($"Расстояние Дамерау-Левенштейна: {distance}");
+            Console.WriteLine();
+        }
+    }
+
+    public static int Distance(string str1Param, string str2Param)
+    {
+        if ((str1Param == null) || (str2Param == null)) return -1;
+
+        int str1Len = str1Param.Length;
+        int str2Len = str2Param.Length;
+
+        if ((str1Len == 0) && (str2Len == 0)) return 0;
+        if (str1Len == 0) return str2Len;
+        if (str2Len == 0) return str1Len;
+
+        string str1 = str1Param.ToUpper();
+        string str2 = str2Param.ToUpper();
+
+        int[,] matrix = new int[str1Len + 1, str2Len + 1];
+
+        for (int i = 0; i <= str1Len; i++)
+            matrix[i, 0] = i;
+
+        for (int j = 0; j <= str2Len; j++)
+            matrix[0, j] = j;
+
+        for (int i = 1; i <= str1Len; i++)
+        {
+            for (int j = 1; j <= str2Len; j++)
+            {
+                int symbEqual =
+                    (str1[i - 1] == str2[j - 1]) ? 0 : 1;
+
+                int ins = matrix[i, j - 1] + 1;
+                int del = matrix[i - 1, j] + 1;
+                int subst = matrix[i - 1, j - 1] + symbEqual;
+
+                matrix[i, j] = Math.Min(Math.Min(ins, del), subst);
+
+                if (i > 1 && j > 1 &&
+                    str1[i - 1] == str2[j - 2] &&
+                    str1[i - 2] == str2[j - 1])
+                {
+                    matrix[i, j] = Math.Min(matrix[i, j],
+                        matrix[i - 2, j - 2] + symbEqual);
+                }
+            }
+        }
+
+        return matrix[str1Len, str2Len];
+    }
+}
